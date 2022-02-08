515d57806ad04a76431c5135659a248fd2444df3
pramitapal <pramita66@gmail.com>
2022-02-08 11:12:04 +0530

    Code cleanup

--- routes/places.js
+++ routes/places.js
@@ -4,9 +4,6 @@ const PlacesController = require("../controllers/PlacesController");
 var router = express.Router();
 
 router.get("/", PlacesController.placeList);
-router.post("/nearByPlaces", PlacesController.placeListByDistanceFromGeopoint);
-// router.post("/", PlacesController.bookStore);
-// router.put("/:id", PlacesController.bookUpdate);
-// router.delete("/:id", PlacesController.bookDelete);
+router.get("/getAllNearByPOI", PlacesController.placeListByDistanceFromGeopoint);
 
 module.exports = router;
